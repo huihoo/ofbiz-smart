9a3beb6674b3338d191aee04a792f2b1c892f049
huangbaihua <huangbaihua@vip.qq.com>
2015-05-01 18:11:26 +0800

    弃用Debug.logWaring方法.改由Debug.logWarn方法；

--- ofbiz-smart/src/main/java/org/huihoo/ofbiz/smart/base/utils/Debug.java
+++ ofbiz-smart/src/main/java/org/huihoo/ofbiz/smart/base/utils/Debug.java
@@ -1,20 +1,16 @@
 /*******************************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *******************************************************************************/
 package org.huihoo.ofbiz.smart.base.utils;
 
@@ -52,8 +48,7 @@ public class Debug {
     }
     switch (level) {
       case Debug.TRACE:
-        if(!logger.isTraceEnabled())
-          return ;
+        if (!logger.isTraceEnabled()) return;
         if (t == null)
           logger.trace(msg);
         else
@@ -78,8 +73,7 @@ public class Debug {
           logger.error(msg, t);
         break;
       case Debug.DEBUG:
-        if(!logger.isDebugEnabled())
-          return ;
+        if (!logger.isDebugEnabled()) return;
         if (t == null)
           logger.debug(msg);
         else
@@ -143,14 +137,33 @@ public class Debug {
     log(Debug.WARING, module, msg, t, params);
   }
 
+
+
+  public static void logWarn(Throwable t, String msg, String module) {
+    log(Debug.WARING, module, msg, t, EMPTY_OBJECT_ARRAY);
+  }
+
+
+  public static void logWarn(String msg, String module) {
+    log(Debug.WARING, module, msg, null, EMPTY_OBJECT_ARRAY);
+  }
+
+
+  public static void logWarn(String msg, String module, Object... params) {
+    log(Debug.WARING, module, msg, null, params);
+  }
+
+  @Deprecated
   public static void logWaring(Throwable t, String msg, String module) {
     log(Debug.WARING, module, msg, t, EMPTY_OBJECT_ARRAY);
   }
 
+  @Deprecated
   public static void logWaring(String msg, String module) {
     log(Debug.WARING, module, msg, null, EMPTY_OBJECT_ARRAY);
   }
 
+  @Deprecated
   public static void logWaring(String msg, String module, Object... params) {
     log(Debug.WARING, module, msg, null, params);
   }
