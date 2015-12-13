7de8221b8a1b21d089f3c012012f096f5eb4b15a
huangbaihua <huangbaihua001@163.com>
2015-12-13 22:58:57 +0800

    RequestHandler接口及其三人默认实现提交

---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/HttpApiRequestHandler.java
@@ -0,0 +1,12 @@
+package org.huihoo.ofbiz.smart.webapp;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpApiRequestHandler implements RequestHandler{
+    @Override
+    public void handleRequest(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/RequestHandler.java
@@ -0,0 +1,8 @@
+package org.huihoo.ofbiz.smart.webapp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface RequestHandler {
+    void handleRequest(HttpServletRequest req, HttpServletResponse resp);
+}
