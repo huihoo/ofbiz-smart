0ef6ae6a3bf0e3861e281ef7bc51fdf3fd8085c2
keepwalking <84271855@qq.com>
2015-12-21 15:43:31 +0800

    增加缓存管理

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/base/cache/Cache.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/base/cache/Cache.java
@@ -2,17 +2,27 @@ package org.huihoo.ofbiz.smart.base.cache;
 
 import java.util.Map;
 
+/**
+ * 缓存接口
+ * 
+ * @author huangbaihua
+ *
+ * @param <K>
+ * @param <V>
+ */
 public interface Cache<K, V> {
-
+  /** 默认存活时间 */
   public final static int DEFAULT_TIME_TO_LIVE_SECONDS = 60;
-  public final static int DEFAULT_TIME_TO_IDLE_SECONDS = 60;
+  /** 默认最大缓存个数 */
   public final static int DEFAULT_MAX_ENTRIES = 1024;
 
   public final static EvictionStrategy LEAST_RECENTLY_USED = EvictionStrategy.LEAST_RECENTLY_USED;
   public final static EvictionStrategy LEAST_FREQUENTLY_USED = EvictionStrategy.LEAST_FREQUENTLY_USED;
   public final static EvictionStrategy LEAST_RECENTLY_ADDED = EvictionStrategy.LEAST_RECENTLY_ADDED;
 
-  enum EvictionStrategy { LEAST_RECENTLY_USED, LEAST_FREQUENTLY_USED, LEAST_RECENTLY_ADDED }
+  enum EvictionStrategy {
+    LEAST_RECENTLY_USED, LEAST_FREQUENTLY_USED, LEAST_RECENTLY_ADDED
+  }
 
   void setEvictionStrategy(EvictionStrategy evictionStrategy);
 
@@ -26,11 +36,9 @@ public interface Cache<K, V> {
 
   void setTimeToLiveSeconds(int timeToLiveSeconds);
 
-  void setTimeToIdleSeconds(int timeToIdleSeconds);
-
   String getName();
 
-  V put(K key, V value,int liveInSeconds);
+  V put(K key, V value, int liveInSeconds);
 
 
   V put(K key, V value);
---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/base/cache/SimpleCacheManager.java
@@ -0,0 +1,94 @@
+package org.huihoo.ofbiz.smart.base.cache;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.huihoo.ofbiz.smart.base.C;
+import org.huihoo.ofbiz.smart.base.location.FlexibleLocation;
+import org.huihoo.ofbiz.smart.base.util.Log;
+
+/**
+ * <p>
+ * 缓存管理器
+ * </p>
+ * 
+ * @author huangbaihua
+ * @sice 1.0
+ *
+ */
+public class SimpleCacheManager {
+  private final static String TAG = SimpleCacheManager.class.getName();
+
+  private volatile static String cacheProviderName = "";
+
+  private volatile static int timeToLiveSeconds;
+
+  private volatile static int maxEntries;
+
+  private final static Map<String, Cache<?, ?>> CACHES_MAP = new ConcurrentHashMap<>();
+
+
+  static {
+    Properties prop = new Properties();
+    try {
+      prop.load(FlexibleLocation.resolveLocation(C.APPLICATION_CONFIG_NAME).openStream());
+      cacheProviderName = prop.getProperty(C.CACHE_PROVIDER_NAME, "org.huihoo.ofbiz.smart.base.cache.DefaultCache");
+      timeToLiveSeconds = Integer.valueOf(prop.getProperty(C.CACHE_DEFAULT_TIMETOLIVESECONDS, "60"));
+      maxEntries = Integer.valueOf(prop.getProperty(C.CACHE_DEFAULT_MAXENTRIES, "1024"));
+      Log.i("Using cache provider is : " + cacheProviderName, TAG);
+    } catch (MalformedURLException e) {
+      Log.e(e, "Unable to load application config properties.", TAG);
+    } catch (IOException e) {
+      Log.e(e, "Unable to load application config properties.", TAG);
+    }
+  }
+
+  public static Cache<?, ?> createCache(String name) {
+    Cache<?, ?> cache = CACHES_MAP.get(name);
+    if (cache == null) {
+      cache = new DefaultCache<>();
+      cache.setTimeToLiveSeconds(timeToLiveSeconds);
+      cache.setMaxEntries(maxEntries);
+      cache.start(name);
+      CACHES_MAP.put(name, cache);
+    }
+    return cache;
+  }
+
+  public static void shutDown(String name) {
+    Cache<?, ?> cache = CACHES_MAP.get(name);
+    if (cache != null) {
+      cache.shutDown();
+      CACHES_MAP.remove(name);
+    }
+  }
+
+  public static void clear(String name) {
+    Cache<?, ?> cache = CACHES_MAP.get(name);
+    if (cache != null) {
+      cache.clear();
+    }
+  }
+
+
+  public static Cache<?, ?> getCache(String name) {
+    Cache<?, ?> cache = CACHES_MAP.get(name);
+    return cache;
+  }
+
+  public static List<Cache<?, ?>> getAllCache() {
+    List<Cache<?, ?>> cacheList = new ArrayList<>();
+    Iterator<Entry<String, Cache<?, ?>>> iter = CACHES_MAP.entrySet().iterator();
+    while (iter.hasNext()) {
+      cacheList.add(iter.next().getValue());
+    }
+    return cacheList;
+  }
+}
