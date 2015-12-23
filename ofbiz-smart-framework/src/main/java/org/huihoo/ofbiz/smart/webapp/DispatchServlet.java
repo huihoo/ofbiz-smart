3229074f85658b705ecea580f848d277ebef951e
huangbaihua <huangbaihua001@163.com>
2015-12-23 21:35:17 +0800

    Add Init Parameters.

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/DispatchServlet.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/DispatchServlet.java
@@ -15,60 +15,87 @@ import org.huihoo.ofbiz.smart.base.C;
 import org.huihoo.ofbiz.smart.base.cache.Cache;
 import org.huihoo.ofbiz.smart.base.cache.SimpleCacheManager;
 import org.huihoo.ofbiz.smart.base.location.FlexibleLocation;
+import org.huihoo.ofbiz.smart.base.util.CommUtil;
 import org.huihoo.ofbiz.smart.base.util.Log;
 import org.huihoo.ofbiz.smart.entity.Delegator;
 import org.huihoo.ofbiz.smart.entity.EbeanDelegator;
 import org.huihoo.ofbiz.smart.entity.GenericEntityException;
 import org.huihoo.ofbiz.smart.service.GenericServiceException;
 import org.huihoo.ofbiz.smart.service.ServiceDispatcher;
-import org.huihoo.ofbiz.smart.webapp.ActionModel.Response;
-import org.huihoo.ofbiz.smart.webapp.ActionModel.ServiceCall;
 import org.huihoo.ofbiz.smart.webapp.handler.ApiDocRequestHandler;
 import org.huihoo.ofbiz.smart.webapp.handler.DefaultRequestHandler;
 import org.huihoo.ofbiz.smart.webapp.handler.HttpApiRequestHandler;
 import org.huihoo.ofbiz.smart.webapp.handler.RequestHandler;
 import org.huihoo.ofbiz.smart.webapp.handler.RestfulRequestHandler;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 
 public class DispatchServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
-  
-  private static final  String TAG = DispatchServlet.class.getName();
+
+  private static final String TAG = DispatchServlet.class.getName();
 
   private static volatile Cache<String, RequestHandler> HANDLER_CACHE;
 
+  /** JSP界面所在跟目录 */
+  private volatile String jspViewBathPath;
+  /** 请求uri后缀 */
+  private volatile String uriSuffix;
+  /** http api 类型的请求uri根 */
+  private volatile String httpApiUrlBase;
+  /** rest api 类型的请求uri根 */
+  private volatile String restApiUrlBase;
+  /** api 文档请求uri根 */
+  private volatile String apiDocUriBase;
+  
+
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
     processRequest(req, resp);
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
     processRequest(req, resp);
   }
 
   protected final void processRequest(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    String targetUri = request.getRequestURI();
+          throws ServletException, IOException {
+    String targetUri = request.getRequestURI().substring(request.getContextPath().length());
     long startTime = System.currentTimeMillis();
     Throwable failureCause = null;
     try {
       RequestHandler requestHandler = null;
-      if (targetUri.startsWith("/rest/")) {
+      if (targetUri.startsWith(restApiUrlBase)) {
         requestHandler = HANDLER_CACHE.get("Rest");
-      } else if (targetUri.startsWith("/api/")) {
+        if (requestHandler == null) {
+          requestHandler = new RestfulRequestHandler();
+          HANDLER_CACHE.put("Rest", requestHandler);
+        }
+      } else if (targetUri.startsWith(httpApiUrlBase)) {
         requestHandler = HANDLER_CACHE.get("Api");
-      } else if (targetUri.startsWith("/api_doc/")) {
+        if (requestHandler == null) {
+          requestHandler = new HttpApiRequestHandler();
+          HANDLER_CACHE.put("Api", requestHandler);
+        }
+
+      } else if (targetUri.startsWith(apiDocUriBase)) {
         requestHandler = HANDLER_CACHE.get("ApiDoc");
+        if (requestHandler == null) {
+          requestHandler = new ApiDocRequestHandler();
+          HANDLER_CACHE.put("ApiDoc", requestHandler);
+        }
       } else {
         requestHandler = HANDLER_CACHE.get("Default");
+        if (requestHandler == null) {
+          requestHandler = new DefaultRequestHandler();
+          HANDLER_CACHE.put("Default", requestHandler);
+        }
       }
       
       requestHandler.handleRequest(request, response);
-      
+
     } catch (ServletException ex) {
       failureCause = ex;
       throw ex;
@@ -78,49 +105,68 @@ public class DispatchServlet extends HttpServlet {
     } finally {
       long processingTime = System.currentTimeMillis() - startTime;
       if (failureCause != null) {
-        Log.i("Process request [" + targetUri + "] cost [" + processingTime + "] ms. But an error happend : " + failureCause.getMessage(), TAG);
+        Log.i("Process request [" + targetUri + "] cost [" + processingTime
+                + "] ms. But an error happend : " + failureCause.getMessage(), TAG);
       } else {
         Log.i("Process request [" + targetUri + "] cost [" + processingTime + "] ms", TAG);
       }
     }
   }
 
+
+
   @SuppressWarnings("unchecked")
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
+    jspViewBathPath = config.getInitParameter("jsp-view-base-path");
+    uriSuffix = config.getInitParameter("uri-suffix");
+    httpApiUrlBase = config.getInitParameter("http-api-uri-base");
+    restApiUrlBase = config.getInitParameter("rest-api-uri-base");
+    apiDocUriBase = config.getInitParameter("api-doc-uri-base");
     
-    initWebContext(config.getServletContext());
-    
-    HANDLER_CACHE = (Cache<String, RequestHandler>) SimpleCacheManager.createCache("RequestHandler-Cache");
+    if (CommUtil.isEmpty(jspViewBathPath)) {
+      jspViewBathPath = "/WEB-INF/views";
+    }
 
-    loadBuiltInHandler();
+    if (CommUtil.isEmpty(uriSuffix)) {
+      uriSuffix = "";
+    }
+
+    if (CommUtil.isEmpty(httpApiUrlBase)) {
+      httpApiUrlBase = "/api/router";
+    }
+
+    if (CommUtil.isEmpty(restApiUrlBase)) {
+      httpApiUrlBase = "/rest";
+    }
+
+    initWebContext(config.getServletContext());
+
+    HANDLER_CACHE =
+            (Cache<String, RequestHandler>) SimpleCacheManager.createCache("RequestHandler-Cache");
+
   }
 
-  protected void initWebContext(ServletContext servletContext) throws ServletException{
+  protected void initWebContext(ServletContext servletContext) throws ServletException {
     try {
       Delegator delegator = new EbeanDelegator();
       ServiceDispatcher serviceDispatcher = new ServiceDispatcher(delegator);
       Properties applicationConfig = new Properties();
-      applicationConfig.load(FlexibleLocation.resolveLocation(C.APPLICATION_CONFIG_NAME).openStream());
-      servletContext.setAttribute(C.CTX_DELETAGOR,delegator);
+      applicationConfig
+              .load(FlexibleLocation.resolveLocation(C.APPLICATION_CONFIG_NAME).openStream());
+      servletContext.setAttribute(C.CTX_DELETAGOR, delegator);
       servletContext.setAttribute(C.CTX_SERVICE_DISPATCHER, serviceDispatcher);
       servletContext.setAttribute(C.APPLICATION_CONFIG_PROP_KEY, applicationConfig);
     } catch (GenericEntityException e) {
-      throw new ServletException("Unable to initialize Delegator."); 
+      throw new ServletException("Unable to initialize Delegator.");
     } catch (GenericServiceException e) {
-      throw new ServletException("Unable to initialize ServiceDispatcher."); 
+      throw new ServletException("Unable to initialize ServiceDispatcher.");
     } catch (MalformedURLException e) {
-      throw new ServletException("Unable to load gobal config file : " + C.APPLICATION_CONFIG_NAME); 
+      throw new ServletException("Unable to load gobal config file : " + C.APPLICATION_CONFIG_NAME);
     } catch (IOException e) {
-      throw new ServletException("Unable to load gobal config file : " + C.APPLICATION_CONFIG_NAME); 
+      throw new ServletException("Unable to load gobal config file : " + C.APPLICATION_CONFIG_NAME);
     }
   }
-  
-  protected void loadBuiltInHandler() {
-    HANDLER_CACHE.put("Default", new DefaultRequestHandler());
-    HANDLER_CACHE.put("Api", new HttpApiRequestHandler());
-    HANDLER_CACHE.put("Rest", new RestfulRequestHandler());
-    HANDLER_CACHE.put("ApiDoc", new ApiDocRequestHandler());
-  }
+
 }
