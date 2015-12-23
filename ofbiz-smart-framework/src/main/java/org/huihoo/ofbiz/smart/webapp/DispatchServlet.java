bda1521013638174e20517c1071642184c87d31b
keepwalking <84271855@qq.com>
2015-12-23 15:13:00 +0800

    Update RequestHandler

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/DispatchServlet.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/DispatchServlet.java
@@ -8,23 +8,81 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.huihoo.ofbiz.smart.base.cache.Cache;
+import org.huihoo.ofbiz.smart.base.cache.SimpleCacheManager;
+import org.huihoo.ofbiz.smart.base.util.Log;
+import org.huihoo.ofbiz.smart.webapp.handler.ApiDocRequestHandler;
+import org.huihoo.ofbiz.smart.webapp.handler.DefaultRequestHandler;
+import org.huihoo.ofbiz.smart.webapp.handler.HttpApiRequestHandler;
+import org.huihoo.ofbiz.smart.webapp.handler.RequestHandler;
+import org.huihoo.ofbiz.smart.webapp.handler.RestfulRequestHandler;
+
 public class DispatchServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
   
+  private static final  String TAG = DispatchServlet.class.getName();
+
+  private static volatile Cache<String, RequestHandler> HANDLER_CACHE;
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    super.doGet(req, resp);
+    processRequest(req, resp);
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    super.doPost(req, resp);
+    processRequest(req, resp);
   }
 
+  protected final void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String targetUri = request.getRequestURI();
+    long startTime = System.currentTimeMillis();
+    Throwable failureCause = null;
+    try {
+      RequestHandler requestHandler = null;
+      if (targetUri.startsWith("/rest/")) {
+        requestHandler = HANDLER_CACHE.get("Rest");
+      } else if (targetUri.startsWith("/api/")) {
+        requestHandler = HANDLER_CACHE.get("Api");
+      } else if (targetUri.startsWith("/api_doc/")) {
+        requestHandler = HANDLER_CACHE.get("ApiDoc");
+      } else {
+        requestHandler = HANDLER_CACHE.get("Default");
+      }
+      
+      requestHandler.handleRequest(request, response);
+
+    } catch (ServletException ex) {
+      failureCause = ex;
+      throw ex;
+    } catch (IOException ex) {
+      failureCause = ex;
+      throw ex;
+    } finally {
+      long processingTime = System.currentTimeMillis() - startTime;
+      if (failureCause != null) {
+        Log.i("Process request [" + targetUri + "] cost [" + processingTime + "] ms. But an error happend : " + failureCause.getMessage(), TAG);
+      } else {
+        Log.i("Process request [" + targetUri + "] cost [" + processingTime + "] ms", TAG);
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked")
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
-    
-    
+
+    HANDLER_CACHE = (Cache<String, RequestHandler>) SimpleCacheManager.createCache("RequestHandler-Cache");
+
+    loadBuiltInHandler();
+  }
+
+  protected void loadBuiltInHandler() {
+    HANDLER_CACHE.put("Default", new DefaultRequestHandler());
+    HANDLER_CACHE.put("Api", new HttpApiRequestHandler());
+    HANDLER_CACHE.put("Rest", new RestfulRequestHandler());
+    HANDLER_CACHE.put("ApiDoc", new ApiDocRequestHandler());
   }
 }
--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/handler/RestfulRequestHandler.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/handler/RestfulRequestHandler.java
@@ -1,12 +1,15 @@
 package org.huihoo.ofbiz.smart.webapp.handler;
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RestfulRequestHandler implements RequestHandler {
   @Override
-  public void handleRequest(HttpServletRequest req, HttpServletResponse resp) {
+  public void handleRequest(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException{
 
   }
 }
