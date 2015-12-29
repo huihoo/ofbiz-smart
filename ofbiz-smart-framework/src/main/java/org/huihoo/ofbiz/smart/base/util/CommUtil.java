1ee418a445e5cff323df3bc94607ad69e637397e
keepwalking <84271855@qq.com>
2015-12-29 16:55:50 +0800

    增加friendlyTime方法

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/base/util/CommUtil.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/base/util/CommUtil.java
@@ -16,7 +16,9 @@ package org.huihoo.ofbiz.smart.base.util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -33,7 +35,13 @@ public class CommUtil {
   public final static Class<?>[] EMPTY_CLAZZ = new Class<?>[] {};
   public static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
   private final static String tag = CommUtil.class.getName();
-
+  
+  private final static ThreadLocal<SimpleDateFormat> yyyyMMddFormater = new ThreadLocal<SimpleDateFormat>() {
+    @Override
+    protected SimpleDateFormat initialValue() {
+      return new SimpleDateFormat("yyyy-MM-dd");
+    }
+  };
 
   public static boolean isNumber(String input) {
     if (isEmpty(input)) return false;
@@ -260,5 +268,50 @@ public class CommUtil {
       }
     }
     return false;
+  }
+
+  
+
+  public static String friendlyTime(long time) {
+    String ftime = "";
+    Calendar cal = Calendar.getInstance();
+
+    // 判断是否是同一天
+    String curDate = yyyyMMddFormater.get().format(cal.getTime());
+    String paramDate = yyyyMMddFormater.get().format(time);
+    if (curDate.equals(paramDate)) {
+      int hour = (int) ((cal.getTimeInMillis() - time) / 3600000);
+      if (hour == 0)
+        ftime = Math.max((cal.getTimeInMillis() - time) / 60000, 1) + "分钟前";
+      else
+        ftime = hour + "小时前";
+      return ftime;
+    }
+
+    long lt = time / 86400000;
+    long ct = cal.getTimeInMillis() / 86400000;
+    int days = (int) (ct - lt);
+    if (days == 0) {
+      int hour = (int) ((cal.getTimeInMillis() - time) / 3600000);
+      if (hour == 0)
+        ftime = Math.max((cal.getTimeInMillis() - time) / 60000, 1) + "分钟前";
+      else
+        ftime = hour + "小时前";
+    } else if (days == 1) {
+      ftime = "昨天";
+    } else if (days == 2) {
+      ftime = "前天 ";
+    } else if (days > 2 && days < 31) {
+      ftime = days + "天前";
+    } else if (days >= 31 && days <= 2 * 31) {
+      ftime = "一个月前";
+    } else if (days > 2 * 31 && days <= 3 * 31) {
+      ftime = "2个月前";
+    } else if (days > 3 * 31 && days <= 4 * 31) {
+      ftime = "3个月前";
+    } else {
+      ftime = yyyyMMddFormater.get().format(time);
+    }
+    return ftime;
   }
 }
