3869581a2d3fc4f118d512d101a4c414c6b89135
keepwalking <84271855@qq.com>
2016-01-05 10:40:16 +0800

    增加CaptchaView

---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/view/CaptchaView.java
@@ -0,0 +1,21 @@
+package org.huihoo.ofbiz.smart.webapp.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CaptchaView implements View {
+
+  @Override
+  public String getContentType() {
+    return null;
+  }
+
+  @Override
+  public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+      throws ViewException {
+
+  }
+
+}
