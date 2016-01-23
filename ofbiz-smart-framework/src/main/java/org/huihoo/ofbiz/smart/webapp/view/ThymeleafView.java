e9416dad5a854f436a2b7cde9bb17b204b67a2c5
huangbaihua <huangbaihua001@163.com>
2016-01-24 00:23:08 +0800

    增加ThymeleafView

---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/webapp/view/ThymeleafView.java
@@ -0,0 +1,23 @@
+package org.huihoo.ofbiz.smart.webapp.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.huihoo.ofbiz.smart.base.C;
+
+public class ThymeleafView implements View {
+
+  @Override
+  public String getContentType() {
+    return C.DEFAULT_CONTENT_TYPE;
+  }
+
+  @Override
+  public void render(Map<String, Object> model, HttpServletRequest request,
+          HttpServletResponse response) throws ViewException {
+    //TODO
+  }
+
+}
